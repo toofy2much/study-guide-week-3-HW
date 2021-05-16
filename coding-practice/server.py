266cdc42ad8d908674ee396822d1834481649ac5
Tuesday Kay <tuesday.kay@gmail.com>
2021-05-16 22:57:09 +0000

    revise debug see get save/name

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -16,12 +16,28 @@ def show_homepage():
 
 @app.route('/form')
 def show_form():
-    pass
+    return render_template('form.html')
 
 @app.route('/results')
 def show_results():
-    pass
+    cheery = request.arg.get('cheery')
+    honest =request.arg.get('honest')
+    dreary = request.arg.get('dreary')
 
+    if cheery:
+        msg = "pin a rose on your nose"
+    if honest:
+        msg = "your no Pinochio"
+    if dreary:
+        msg = "tomorow is a new day"
+
+    return render_template('results.html', msg=msg)
+
+@app.route('/save-name' ,methods= ["Post"])
+def save_name():
+    name = request.form.get('name')
+    session['name'] = name
+    return render_template('/save-name' ,methods = ["Post"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
